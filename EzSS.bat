d8112b3a212e8b28cf35f2aa2ff5d731c974008e
iNotAtch <131803570+AtchYT@users.noreply.github.com>
2024-11-25 22:29:30 +0100

    ✔️ » Updated | v2.0.0

--- EzSS.bat
+++ EzSS.bat
@@ -1,4 +1,4 @@
 @echo off
-title EzSS - v1.1.2
+title EzSS - v2.0.0
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0EzSS.ps1""' -Verb RunAs}"
